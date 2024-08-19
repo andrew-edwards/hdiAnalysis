c0762d3c3cc1ebcf1dd7b45a8cafcbe9da632ded
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-08-19 11:14:30 -0700

    Update help on create_intervals and the two input-dependent functions, before merging into one help file.

--- R/create-intervals.R
+++ R/create-intervals.R
@@ -1,7 +1,13 @@
-##' Make `create_intervals()` use the right method depending on class of input
+##' Generic function to make `create_intervals()` use the right method depending on class of input
 ##'
-##' Based on `plot()`. The functions are `create_itervals.numeric()` and `create_intervals.data.frame()`.
-##' @param dat
+##' Based on `plot()` and `plot.default()`. The functions are
+##'  `create_intervals.numeric()` and `create_intervals.data.frame()` for,
+##'  respectively, a vector of numeric values and a data frame of values. See
+##'  those respective helpfiles.
+##' @param dat if a numeric vector of values then [create_intervals.numeric()]
+##'  gets called, whereas if a data frame then [create_intervals.data.frame()]
+##'  gets used.
+##' @param ... arguments to pass onto [create_intervals.numeric()] or [create_intervals.data.frame()].
 ##' @return
 ##' @export
 ##' @author Andrew Edwards
@@ -9,6 +15,7 @@
 ##' \dontrun{
 ##'
 ##' }
-create_intervals <- function(dat, ...){
+create_intervals <- function(dat,
+                             ...){
   UseMethod("create_intervals")
 }
