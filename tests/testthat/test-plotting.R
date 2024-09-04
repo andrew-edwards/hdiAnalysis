6952d633da526821cfe362abf254e3c8fc4044da
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-09-04 10:22:27 -0700

    Remove figure pdf's after creation during testing.

--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -1,3 +1,6 @@
-test_that("figure 1 generation code works", {
+test_that("figures 1 and 2 generation code works", {
   expect_invisible(figure_1())
+  file.remove("figure-1.pdf")
+  expect_invisible(figure_2())
+  file.remove("figure-2.pdf")
 })
