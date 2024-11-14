417d49e2c3593f5235a7abafcd7580969bbd0102
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2024-11-13 16:21:36 -0800

    Add final test to get code coverage to 100%.

--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -1,6 +1,8 @@
-test_that("figures 1 and 2 generation code works", {
+test_that("figures 1, 2, and S1 generation code works", {
   expect_invisible(figure_1())
   file.remove("figure-1.pdf")
   expect_invisible(figure_2())
   file.remove("figure-2.pdf")
+  expect_invisible(figure_s1())
+  file.remove("figure-s1.pdf")
 })
