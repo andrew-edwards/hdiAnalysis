970d199b5578caccf0f1af655f5749f5569d0843
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2025-04-02 11:57:36 -0700

    Add tests for remaining options of plot.intervals_density_list().

--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -6,3 +6,28 @@ test_that("figures 1, 2, and S1 generation code works", {
   expect_invisible(figure_s1())
   file.remove("figure-s1.pdf")
 })
+
+test_that("remaining plotting functions/options work", {
+  res_all_years_short <- create_intervals(dplyr::select(hake_recruitment_mcmc,
+                                                        `1966`:`1969`))
+  expect_invisible(plot(res_all_years_short,
+                        add_line = 5))
+  expect_invisible(plot(res_all_years_short,
+                        type = "separate"))
+
+  expect_invisible(plot(res_all_years_short,
+                        type = "separate",
+                        join_intervals = TRUE))
+
+  expect_invisible(plot(res_all_years_short,
+                        type = "separate",
+                        add_line_at_0.4 = TRUE))
+
+  expect_invisible(plot(res_all_years_short,
+                        type = "separate",
+                        add_line = 0.5))
+
+  expect_invisible(plot(res_all_years_short,
+                        type = "separate",
+                        y_ticks_start = NULL))
+})
