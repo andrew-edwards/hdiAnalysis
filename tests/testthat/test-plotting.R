7245c32f96f96e0d2c0c84f3ddf3c4a7ce8b15b6
andrew-edwards <Andrew.Edwards@dfo-mpo.gc.ca>
2025-04-02 12:06:08 -0700

    Add tests for plot_pointwise_intervals().

--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -7,7 +7,7 @@ test_that("figures 1, 2, and S1 generation code works", {
   file.remove("figure-s1.pdf")
 })
 
-test_that("remaining plotting functions/options work", {
+test_that("remaining options for plot.intervals_density_list() work", {
   res_all_years_short <- create_intervals(dplyr::select(hake_recruitment_mcmc,
                                                         `1966`:`1969`))
   expect_invisible(plot(res_all_years_short,
@@ -30,4 +30,19 @@ test_that("remaining plotting functions/options work", {
   expect_invisible(plot(res_all_years_short,
                         type = "separate",
                         y_ticks_start = NULL))
+})
+
+
+test_that("plot_pointwise__intervals() works", {
+  res_vec <- create_intervals(rec_2021)
+  set.seed(42)
+  vec_sub <- sample(rec_2021, 200)    # then can easily see the 5 at each end
+  res_sub <- create_intervals(vec_sub)
+
+  expect_invisible(plot_pointwise_intervals(rec_2021,
+                                            res_vec))
+
+  expect_invisible(plot_pointwise_intervals(rec_2021,
+                                            res_vec,
+                                            type = "moving_window"))
 })
